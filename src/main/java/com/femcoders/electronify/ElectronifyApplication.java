d79bf98d2a21c2d04abd31252ba565c2a8ce38b8
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-06-23 15:37:00 +0200

    Make structure pof project

---
+++ src/main/java/com/femcoders/electronify/ElectronifyApplication.java
@@ -0,0 +1,13 @@
+package com.femcoders.electronify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/femcoders/electronify/ElectronifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femcoders.electronify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
