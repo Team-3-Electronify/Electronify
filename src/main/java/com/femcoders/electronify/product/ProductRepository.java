32c07997d9a3139954009ec28ef747181c2e2253
PCalvoGarcia <144434991+PCalvoGarcia@users.noreply.github.com>
2025-06-24 14:46:07 +0200

    Revert "Feature/product service"

--- src/main/java/com/femcoders/electronify/product/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.femcoders.electronify.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
