51ab1516baccf96b05449375867f12a676e652ef
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-06-27 11:33:09 +0200

    feat: add method getProductsByCategory_Name

--- src/main/java/com/femcoders/electronify/product/ProductRepository.java
+++ src/main/java/com/femcoders/electronify/product/ProductRepository.java
@@ -2,8 +2,10 @@ package com.femcoders.electronify.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByName(String name);
+    List<Product> getProductsByCategory_Name(String name);
 }
