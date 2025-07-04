b9751f3a88c0e3d781366c1edfe17a1ff49c6b68
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-07-04 15:24:32 +0200

    test: add application-test.properties configuration profile test, data.sql for test and add profile test in ProductServiceTestFilters

--- src/test/java/com/femcoders/electronify/product/ProductServiceTestFilters.java
+++ src/test/java/com/femcoders/electronify/product/ProductServiceTestFilters.java
@@ -7,6 +7,7 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 import java.util.List;
@@ -16,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 public class ProductServiceTestFilters {
     @Autowired
