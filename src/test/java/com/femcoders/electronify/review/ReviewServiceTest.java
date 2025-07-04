fa32433763a7dfd98ab6bba1761a710df955ba84
NepyAnna <nepyann@gmail.com>
2025-07-05 00:18:08 +0200

    test: added createReviewTest() method in ReviewServiceTest

---
+++ src/test/java/com/femcoders/electronify/review/ReviewServiceTest.java
@@ -0,0 +1,89 @@
+package com.femcoders.electronify.review;
+
+import com.femcoders.electronify.product.Product;
+import com.femcoders.electronify.product.ProductRepository;
+import com.femcoders.electronify.product.ProductService;
+import com.femcoders.electronify.review.dto.ReviewRequest;
+import com.femcoders.electronify.review.dto.ReviewResponse;
+import com.femcoders.electronify.user.UserRepository;
+import com.femcoders.electronify.user.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(MockitoExtension.class)
+class ReviewServiceTest {
+    @Mock
+    private ReviewRepository reviewRepository;
+    @Mock
+    private ProductRepository productRepository;
+    @Mock
+    private ProductService productService;
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private ReviewService reviewService;
+
+    private User testUser;
+    private Product testProduct;
+    private Review testReview;
+    private ReviewRequest reviewRequest;
+
+    @BeforeEach
+    public void setUp() {
+        reviewService = Mockito.spy(new ReviewService(reviewRepository,
+                productRepository,
+                userRepository,
+                productService));
+
+        testUser = new User();
+        testUser.setId(10L);
+        testUser.setUsername("testUser");
+
+        testProduct = Product.builder()
+                .id(100L)
+                .name("Test Product")
+                .price(100.0)
+                .imageUrl("img.jpg")
+                .featured(false)
+                .rating(0.0)
+                .reviewCount(0)
+                .build();
+
+        testReview = Review.builder()
+                .id(1L)
+                .rating(4.5)
+                .body("Greate product!")
+                .product(testProduct)
+                .user(testUser)
+                .build();
+
+        reviewRequest = new ReviewRequest(4.5, "Greate product!", 100L);
+    }
+
+    @Test
+    void createReviewTest() {
+        Mockito.doReturn(testUser).when(reviewService).getAuthenticatedUser();
+        Mockito.when(productRepository.findById(100L)).thenReturn(Optional.of(testProduct));
+        Mockito.when(reviewRepository.save(any(Review.class))).thenReturn(testReview);
+
+        ReviewResponse response = reviewService.createReview(reviewRequest);
+
+        assertNotNull(response);
+        assertEquals(4.5, response.rating());
+        assertEquals(1L, response.id());
+        assertEquals("Greate product!", response.body());
+        
+        Mockito.verify(productService).updateProductStats(100L);
+    }
+}
