25090e25effc7413789029a2a867e772b5e646bf
NepyAnna <nepyann@gmail.com>
2025-07-06 02:26:06 +0200

    feat: added SwaggerConfig,ErrorResponse, ApiResponse anotations in CartController

--- src/main/java/com/femcoders/electronify/cart/CartController.java
+++ src/main/java/com/femcoders/electronify/cart/CartController.java
@@ -3,10 +3,14 @@ package com.femcoders.electronify.cart;
 import com.femcoders.electronify.cart.dto.CartRequest;
 import com.femcoders.electronify.cart.dto.CartResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,13 +21,77 @@ public class CartController {
     private final CartService cartService;
 
     @GetMapping
-    @Operation(summary = "Get all products in a cart by user")
+    @Operation(
+            summary = "Get all products in a cart by user",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Cart by username returned",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = CartResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Cart for user {username}  does not exist.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal server error",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    )
+            }
+    )
     public ResponseEntity<CartResponse> getCart() {
         return ResponseEntity.ok(cartService.getCartByUser());
     }
 
     @PostMapping("/add/{productId}")
-    @Operation(summary = "Add new product in a user cart")
+    @Operation(
+            summary = "Add new product in a user cart",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Product added to cart successfully",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = CartResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Product not found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid quantity or bad request",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal server error",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    )
+            }
+    )
     public ResponseEntity<CartResponse> addToCart(
             @PathVariable Long productId,
             @RequestBody CartRequest request) {
