63ab0d68323a511894654dc7ffd52c98ee3a4641
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-07-07 12:18:22 +0200

    fix: CloudinaryConfig problems .env

--- src/main/java/com/femcoders/electronify/cloudinary/CloudinaryConfig.java
+++ src/main/java/com/femcoders/electronify/cloudinary/CloudinaryConfig.java
@@ -11,9 +11,9 @@ public class CloudinaryConfig {
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", System.getenv("CLOUDINARY_CLOUD_NAME"),
-                "api_key", System.getenv("CLOUDINARY_API_KEY"),
-                "api_secret", System.getenv("CLOUDINARY_API_SECRET")
+                "cloud_name","electronify" ,
+                "api_key", "556452817547762",
+                "api_secret", "QCoDx1xRmB55zW-0de1p0ax4tGA"
         ));
     }
 }
